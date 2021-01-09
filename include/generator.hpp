d9a0b1603bf545d7c0770806c3d8ea6f554208e7
Cvelth <cvelth.mail@gmail.com>
2021-01-09 19:57:38 +0200

    Implement 'struct' parser

--- include/generator.hpp
+++ include/generator.hpp
@@ -6,68 +6,32 @@
 
 namespace vma_xml {
 	namespace detail {
-		struct entity_t {
-			std::string_view kind;
-			std::string_view refid;
-		};
 		struct variable_t {
-			std::string_view name;
-			std::string_view refid;
+			std::string name;
+			std::string type;
 		};
 		struct struct_t {
-			std::string_view name;
-			std::string_view refid;
+			std::string name;
 			std::vector<variable_t> variables;
-		};
-		struct define_t {
-			std::string_view name;
-			std::string_view refid;
-		};
-		struct enum_value_t {
-			std::string_view name;
-			std::string_view refid;
-		};
-		struct enum_t {
-			std::string_view name;
-			std::string_view refid;
-			std::vector<enum_value_t> values;
-		};
-		struct typedef_t {
-			std::string_view name;
-			std::string_view refid;
-		};
-		struct function_t {
-			std::string_view name;
-			std::string_view refid;
-		};
-		struct file_t {
-			std::string_view name;
-			std::string_view refid;
-			std::vector<define_t> defines;
-			std::vector<enum_t> enums;
-			std::vector<typedef_t> typedefs;
-			std::vector<function_t> functions;
 		};
 		struct data_t {
 			std::vector<struct_t> structs;
-			std::vector<file_t> files;
 		};
+		
+		std::optional<pugi::xml_document> load_xml(std::filesystem::path const &file);
+		std::optional<variable_t> parse_variable(pugi::xml_node const &xml);
+		bool parse_struct(pugi::xml_node const &xml, detail::data_t &data);
+		bool parse_file(pugi::xml_node const &xml, detail::data_t &data);
+		bool parse_compound(std::string_view refid, std::filesystem::path const &directory, 
+							detail::data_t &data);
 	}
-	detail::entity_t parse_attributes(pugi::xml_node const &input);
 
-	detail::variable_t parse_variable(pugi::xml_node const &input, std::string_view refid);
-	detail::struct_t parse_struct(pugi::xml_node const &input, std::string_view refid);
-
-	detail::define_t parse_define(pugi::xml_node const &input, std::string_view refid);
-	detail::enum_value_t parse_enumvalue(pugi::xml_node const &input, std::string_view refid);
-	detail::enum_t parse_enum(pugi::xml_node const &input, std::string_view refid);
-	detail::typedef_t parse_typedef(pugi::xml_node const &input, std::string_view refid);
-	detail::function_t parse_function(pugi::xml_node const &input, std::string_view refid);
-	detail::file_t parse_file(pugi::xml_node const &input, std::string_view refid);
-
-	std::optional<detail::data_t> parse_index(pugi::xml_document const &index_file);
-
-	std::optional<pugi::xml_document> load_xml(std::filesystem::path const &file);
-
-	std::optional<pugi::xml_document> generate(std::filesystem::path const &directory);
+	std::optional<detail::data_t> parse(std::filesystem::path const &directory);
+	std::optional<pugi::xml_document> generate(detail::data_t const &data);
+	inline std::optional<pugi::xml_document> generate(std::filesystem::path const &directory) {
+		if (auto data = parse(directory); data)
+			return generate(*data);
+		else
+			return std::nullopt;
+	}
 }
--- source/generator.cpp
+++ source/generator.cpp
@@ -4,7 +4,7 @@
 #include <string_view>
 using namespace std::literals;
 
-std::optional<pugi::xml_document> vma_xml::load_xml(std::filesystem::path const &file) {
+std::optional<pugi::xml_document> vma_xml::detail::load_xml(std::filesystem::path const &file) {
 	auto output = std::make_optional<pugi::xml_document>();
 	if (auto result = output->load_file(file.c_str()); result)
 		return output;
@@ -13,151 +13,95 @@ std::optional<pugi::xml_document> vma_xml::load_xml(std::filesystem::path const 
 			<< result.description() << '\n';
 	return std::nullopt;
 }
-vma_xml::detail::entity_t vma_xml::parse_attributes(pugi::xml_node const &input) {
-	vma_xml::detail::entity_t output;
-	for (auto const &attribute : input.attributes())
-		if (attribute.name() == "kind"sv)
-			output.kind = attribute.value();
-		else if (attribute.name() == "refid"sv)
-			output.refid = attribute.value();
+std::optional<vma_xml::detail::variable_t> vma_xml::detail::parse_variable(pugi::xml_node const &xml) {
+	if (auto name = xml.child("name"), type = xml.child("type"); name && type)
+		if (auto child = type.first_child(); child.type() == pugi::xml_node_type::node_pcdata)
+			return std::make_optional<vma_xml::detail::variable_t>(name.child_value(), type.child_value());
+		else if (child.name() == "ref"sv)
+			return std::make_optional<vma_xml::detail::variable_t>(name.child_value(), child.child_value());
 		else
-			std::cout << "Warning: Ignore an unknown attribute: " << attribute.name()
-				<< "=\"" << attribute.value() << "\".\n";
-	return output;
-}
-std::string_view name_only(pugi::xml_node const &input) {
-	std::string_view output;
-	for (auto const &child : input.children())
-		if (child.name() == "name"sv)
-			output = child.child_value();
-		else
-			std::cout << "Warning: Ignore an unknown child: " << child.name() << ".\n";
-	return output;
+			std::cout << "Warning: Ignore a variable with unknown type: '" << name.child_value() << "'.\n";
+		return std::nullopt;
 }
 
-vma_xml::detail::variable_t vma_xml::parse_variable(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::variable_t {
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::struct_t vma_xml::parse_struct(pugi::xml_node const &input, std::string_view refid) {
-	vma_xml::detail::struct_t output; output.refid = refid;
-	for (auto const &child : input.children())
-		if (child.name() == "name"sv)
+bool vma_xml::detail::parse_struct(pugi::xml_node const &xml, detail::data_t &data) {
+	struct_t output;
+	for (auto &child : xml.children()) {
+		if (child.name() == "compoundname"sv)
 			output.name = child.child_value();
-		else if (child.name() == "member"sv)
-			if (auto entity = parse_attributes(child); entity.kind == "variable"sv)
-				output.variables.emplace_back(parse_variable(child, entity.refid));
-			else
-				std::cout << "Warning: Ignore a struct member: '" 
-					<< entity.kind << "'. Only variables are supported.\n";
-		else
-			std::cout << "Warning: Ignore an unknown child: " << child.name() << ".\n";
-	return output;
-}
-
-vma_xml::detail::define_t vma_xml::parse_define(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::define_t {
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::enum_t vma_xml::parse_enum(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::enum_t{
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::enum_value_t vma_xml::parse_enumvalue(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::enum_value_t {
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::typedef_t vma_xml::parse_typedef(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::typedef_t {
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::function_t vma_xml::parse_function(pugi::xml_node const &input, std::string_view refid) {
-	return vma_xml::detail::function_t{
-		.name = name_only(input),
-		.refid = refid
-	};
-}
-vma_xml::detail::file_t vma_xml::parse_file(pugi::xml_node const &input, std::string_view refid) {
-	detail::file_t output; output.refid = refid;
-	for (auto const &child : input.children())
-		if (child.name() == "name"sv)
-			output.name = child.child_value();
-		else if (child.name() == "member"sv)
-			if (auto entity = parse_attributes(child); entity.kind == "define"sv)
-				output.defines.emplace_back(parse_define(child, entity.refid));
-			else if (entity.kind == "enum"sv)
-				output.enums.emplace_back(parse_enum(child, entity.refid));
-			else if (entity.kind == "enumvalue"sv)
-				if (!output.enums.empty())
-					output.enums.back().values.emplace_back(
-						parse_enumvalue(child, entity.refid)
-					);
+		else if (child.name() == "sectiondef"sv)
+			for (auto &member : child.children())
+				if (member.name() == "memberdef"sv)
+					if (auto kind = member.attribute("kind").value(); kind == "variable"sv) {
+						if (auto variable = parse_variable(member); variable)
+							output.variables.emplace_back(*variable);
+					} else
+						std::cout << "Warning: Ignore a struct member: '" << kind 
+							<< "'. Only variables are supported.\n";
 				else
-					std::cout << "Warning: Ignore enumvalue: no enums were defined.";
-			else if (entity.kind == "typedef"sv)
-				output.typedefs.emplace_back(parse_typedef(child, entity.refid));
-			else if (entity.kind == "function"sv)
-				output.functions.emplace_back(parse_function(child, entity.refid));
-			else
-				std::cout << "Warning: Ignore an unknown file member: '"
-					<< entity.kind << "'.\n";
-		else
-			std::cout << "Warning: Ignore an unknown child: " << child.name() << ".\n";
-	return output;
-}
-
-std::optional<vma_xml::detail::data_t> vma_xml::parse_index(pugi::xml_document const &index_file) {
-	if (auto index = index_file.child("doxygenindex"); index) {
-		detail::data_t output;
-		for (auto const &compound : index.children())
-			if (compound.name() == "compound"sv)
-				if (auto entity = parse_attributes(compound); entity.kind == "struct"sv)
-					output.structs.emplace_back(parse_struct(compound, entity.refid));
-				else if (entity.kind == "file"sv)
-					output.files.emplace_back(parse_file(compound, entity.refid));
-				else if (entity.kind == "page"sv || entity.kind == "dir"sv) {
-					// Ignore 'page' and 'dir' index entries.
-				} else
-					std::cout << "Warning: Ignore a compound with an unknown kind: '" 
-						<< entity.kind << "'.\n";
-			else
-				std::cout << "Warning: Ignore unknown node: " << compound.name() << '\n';
-		return output;
-	} else
-		std::cout << "Error: Fail to locate 'doxygenindex' node in the provided input file.";
-	return std::nullopt;
-}
-
-std::optional<pugi::xml_document> vma_xml::generate(std::filesystem::path const &directory) {
-	if (auto index_xml = load_xml(directory.string() + "/index.xml"); index_xml) {
-		auto index_data = parse_index(*index_xml);
-
-
-		auto output = std::make_optional<pugi::xml_document>();
-		auto node = output->append_child("languages");
-
-		auto english = node.append_child("english");
-		english.append_attribute("locale").set_value("us");
-		english.append_child(pugi::node_pcdata).set_value("true");
-
-		node.append_child("日本語").append_child(pugi::node_pcdata).set_value("true");
-		node.append_child("Español").append_child(pugi::node_pcdata).set_value("true");
-
-		return std::move(output);
+					std::cout << "Warning: Ignore unknown struct member: '" << member.name() << "'.\n";
 	}
+
+	if (output.name != "") {
+		data.structs.emplace_back(std::move(output));
+		return true;
+	} else
+		std::cout << "Warning: Ignore a struct compound without a name.\n";
+	return false;
+}
+bool vma_xml::detail::parse_file(pugi::xml_node const &, detail::data_t &) {
+	return false;
+}
+
+bool vma_xml::detail::parse_compound(std::string_view refid, std::filesystem::path const &directory, 
+									 detail::data_t &data) {
+	std::filesystem::path file_path = directory; (file_path /= refid.data()) += ".xml";
+	if (auto compound_xml = detail::load_xml(file_path); compound_xml)
+		if (auto doxygen = compound_xml->child("doxygen"); doxygen)
+			if (auto compound = doxygen.child("compounddef"); compound)
+				if (std::string_view kind = compound.attribute("kind").value(); kind == "struct"sv)
+					return parse_struct(compound, data);
+				else if (kind == "file"sv)
+					return parse_file(compound, data);
+				else
+					std::cout << "Warning: Ignore a compound of an unknown kind: '" << kind << "'.\n";
+	return false;
+}
+
+std::optional<vma_xml::detail::data_t> vma_xml::parse(std::filesystem::path const &directory) {
+	if (auto index_xml = detail::load_xml(directory.string() + "/index.xml"); index_xml)
+		if (auto index = index_xml->child("doxygenindex"); index) {
+			detail::data_t output;
+			for (auto const &compound : index.children())
+				if (compound.name() == "compound"sv)
+					if (auto kind = compound.attribute("kind").value(); kind == "struct"sv || kind == "file"sv)
+						detail::parse_compound(compound.attribute("refid").value(), directory, output);
+					else if (kind == "page"sv || kind == "dir"sv) {
+						// Ignore 'page' and 'dir' index entries.
+					} else
+						std::cout << "Warning: Ignore a compound of an unknown kind: '" << kind << "'.\n";
+				else
+					std::cout << "Warning: Ignore an unknown node: " << compound.name() << '\n';
+			return output;
+		}
 	return std::nullopt;
 }
 
+std::optional<pugi::xml_document> vma_xml::generate([[maybe_unused]] detail::data_t const &data) {
+	auto output = std::make_optional<pugi::xml_document>();
+	auto node = output->append_child("languages");
+
+	auto english = node.append_child("english");
+	english.append_attribute("locale").set_value("us");
+	english.append_child(pugi::node_pcdata).set_value("true");
+
+	node.append_child("日本語").append_child(pugi::node_pcdata).set_value("true");
+	node.append_child("Español").append_child(pugi::node_pcdata).set_value("true");
+
+	return std::move(output);
+}
+
+#ifndef VMA_XML_NO_MAIN
 int main(int argc, char **argv) {
 	std::string_view input_path = "../doxygen_xml/";
 	std::string_view output_path = "../output/";
@@ -169,12 +113,12 @@ int main(int argc, char **argv) {
 		std::cout << "Warning: All the argument after the first two are ignored.\n";
 	if (argc == 1)
 		std::cout << "Warning: No paths provided. Using default values:\n"
-			<< "input index: '" << input_path << "'.\n"
-			<< "output: '" << output_path << "'.\n";
+		<< "input index: '" << input_path << "'.\n"
+		<< "output: '" << output_path << "'.\n";
 
 	if (auto output = vma_xml::generate(input_path); output) {
 		std::filesystem::create_directory(output_path);
-		std::filesystem::path output_file = output_path; output_file /= "xma.xml";
+		std::filesystem::path output_file = output_path; output_file /= "vma.xml";
 		if (auto result = output->save_file(output_file.c_str()); result)
 			std::cout << "\nSuccess: " << std::filesystem::absolute(output_file) << "\n";
 		else
@@ -183,3 +127,89 @@ int main(int argc, char **argv) {
 		std::cout << "Error: Generation failed.";
 	return 0;
 }
+#endif
+
+//vma_xml::detail::variable_t vma_xml::parse_variable(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::variable_t {
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//std::optional<vma_xml::detail::struct_t> vma_xml::detail::parse_struct(pugi::xml_node const &input, 
+//																	   std::string_view refid) {
+//	if (auto index_xml = load_xml(directory.string() + "/index.xml"); index_xml)
+//		if (auto index = index_xml->child("doxygenindex"); index) {
+//	
+//	vma_xml::detail::struct_t output; output.refid = refid;
+//	for (auto const &child : input.children())
+//		if (child.name() == "name"sv)
+//			output.name = child.child_value();
+//		else if (child.name() == "member"sv)
+//			if (auto entity = parse_attributes(child); entity.kind == "variable"sv)
+//				output.variables.emplace_back(parse_variable(child, entity.refid));
+//			else
+//				std::cout << "Warning: Ignore a struct member: '" 
+//					<< entity.kind << "'. Only variables are supported.\n";
+//		else
+//			std::cout << "Warning: Ignore an unknown child: " << child.name() << ".\n";
+//	return output;
+//}
+
+//vma_xml::detail::define_t vma_xml::parse_define(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::define_t {
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//vma_xml::detail::enum_t vma_xml::parse_enum(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::enum_t{
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//vma_xml::detail::enum_value_t vma_xml::parse_enumvalue(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::enum_value_t {
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//vma_xml::detail::typedef_t vma_xml::parse_typedef(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::typedef_t {
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//vma_xml::detail::function_t vma_xml::parse_function(pugi::xml_node const &input, std::string_view refid) {
+//	return vma_xml::detail::function_t{
+//		.name = name_only(input),
+//		.refid = refid
+//	};
+//}
+//vma_xml::detail::file_t vma_xml::parse_file(pugi::xml_node const &input, std::string_view refid) {
+//	detail::file_t output; output.refid = refid;
+//	for (auto const &child : input.children())
+//		if (child.name() == "name"sv)
+//			output.name = child.child_value();
+//		else if (child.name() == "member"sv)
+//			if (auto entity = parse_attributes(child); entity.kind == "define"sv)
+//				output.defines.emplace_back(parse_define(child, entity.refid));
+//			else if (entity.kind == "enum"sv)
+//				output.enums.emplace_back(parse_enum(child, entity.refid));
+//			else if (entity.kind == "enumvalue"sv)
+//				if (!output.enums.empty())
+//					output.enums.back().values.emplace_back(
+//						parse_enumvalue(child, entity.refid)
+//					);
+//				else
+//					std::cout << "Warning: Ignore enumvalue: no enums were defined.";
+//			else if (entity.kind == "typedef"sv)
+//				output.typedefs.emplace_back(parse_typedef(child, entity.refid));
+//			else if (entity.kind == "function"sv)
+//				output.functions.emplace_back(parse_function(child, entity.refid));
+//			else
+//				std::cout << "Warning: Ignore an unknown file member: '"
+//					<< entity.kind << "'.\n";
+//		else
+//			std::cout << "Warning: Ignore an unknown child: " << child.name() << ".\n";
+//	return output;
+//}
